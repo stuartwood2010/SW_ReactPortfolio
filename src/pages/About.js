c7152bb801fb500af9a75a64e8bb6ffefcdd69e0
Stuart Wood <stuartwood2010@hotmail.com>
2022-05-18 17:47:37 -0600

    mobile responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,18 +4,22 @@ function About() {
     return (
     <div id="aboutMe">
       <img id="profileImg" src="https://i.ibb.co/PF3kbCD/Profile-Pic.jpg" alt="Profile of Stuart Wood"/>
-      <content id="bio">
-        <p>
-          My name is Stuart Wood and I am a recent graduate of the University of Denver's Full Stack Coding Bootcamp.
-          I am passionate about making web development dreams come true, It is incredibly rewarding to see a project come to life on the page.
-          I come from a blue-collar background, and am not afraid of hard work or long hours. I was taught to work until the job is done, and that the customer is always right.
-          I am eager to start my career and continue learning in the fast-paced and ever changing tech industry.
-        </p>
-        <p>
-          My other hobbies include gaming, golf, and traveling. 
-          My wife and I are on a mission to visit all 50 states, along with many other countries around the world. 
-        </p>
-        <p>
+      <content>
+        <p id="bio">
+          Hi, I’m Stuart Wood, a recent “A+” graduate of the University of Denver's highly-respected Full Stack Coding Bootcamp. 
+          Success in this program is proof of my technical abilities in the field of web development, but I want to also assure you 
+          that I thrive on the challenges that come with every project and I take great pride in producing efficient and effective 
+          solutions that:  1) check all of the behind-the-scenes boxes, 2) works great for the user, and 3) looks great on-screen.
+          Not that I don’t enjoy the easy stuff, but at the end of the day, solving the tougher puzzles brings me a higher level of satisfaction.
+          I attribute this (in part) to my blue-collar upbringing and a history of getting the job done and done right, to the client’s specs.
+          <br></br>
+          <br></br>
+          Continuing to learn my craft is clearly of critical importance, as very few fields are changing as fast as all aspects of the tech industry,
+          so I’m looking forward to beginning that real-world phase of my education as an active participant, rather than a scholar. Outside of work, and 
+          to offset my established programming and gaming skills, I enjoy being frustrated on the golf course, rooting for the home sports teams, and 
+          doing a bit of traveling now and again, all of which help to recharge/refresh the batteries. 
+          <br></br>
+          <br></br>
           It would be an honor to work with you on something. I look forward to hearing from you!
         </p>
       </content>
